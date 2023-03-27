cdd0dda1345d977d39b51628e8196bb43bfb365a
Lmraza98 <lmraza98@gmail.com>
2023-03-26 22:45:54 -0400

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser()
+
+  const { data } = api.posts.getAll.useQuery()
+  console.log(data)
   return (
     <>
       <Head>
@@ -19,6 +21,11 @@ const Home: NextPage = () => {
         <div>
           {!user.isSignedIn && <SignInButton/>}
           {!!user.isSignedIn && <SignOutButton/>}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
